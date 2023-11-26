9212dcb6d6e0e9b9edb62bd111e40d915f87fa81
“Luis” <mrqueiroz22014@gmail.com>
2023-11-25 21:33:36 -0300

    Finalyzing home page

--- Home.py
+++ Home.py
@@ -2,14 +2,14 @@ import streamlit as st
 from PIL                  import Image
 
 st.set_page_config(
-    page_title="Análise de Sentimentos em Textos: Detecção de Frases Sarcásticas",
-    page_icon="😏",
+    page_title="Análise de Sentimentos em Textos: Detecção de Manchetes de Notícias Sarcásticas",
+    page_icon="📰",
     layout='wide'
 )
 
-st.title("Bem-vindo(a) ao painel do Projeto sobre Detecção de Frases Sarcásticas em textos! 😏")
+st.title("Bem-vindo(a) ao painel do Projeto sobre Detecção de Manchetes de Notícias Sarcásticas ! 🗞️")
 
-image = Image.open('imgs/sentiment.jpeg') 
+image = Image.open('imgs/manchete.jpeg') 
 st.sidebar.image(image)
 
 st.markdown(
@@ -22,5 +22,6 @@ st.markdown(
      
      - Essa abordagem é especialmente utilizada em dados provenientes de redes sociais, avaliações de produtos, comentários online e outras fontes de texto, com o objetivo de compreender o sentimento expresso pelos usuários.
 
+     #### Neste projeto, o objetivo é detectar se uma manchete de uma jornal, por exemplo, possui sarcasmo ou não. A partir disso, podemos inferir sobre a confiabilidade daquela notícia e possíveis objetivos por trás daquela manchete.
     """
 )
